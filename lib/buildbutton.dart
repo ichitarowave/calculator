d5c404136814c20d3e2cff0562f59b780d6c5540
Ichitaro Kume <79429444+ichitarowave@users.noreply.github.com>
2022-06-18 14:47:42 +0900

    buildbuttonのクラスを作成

---
+++ lib/buildbutton.dart
@@ -0,0 +1,29 @@
+// import 'package:flutter/material.dart';
+
+// class BuildButton extends StatelessWidget {
+//   const BuildButton({
+//     required this.onPressed,
+//     required Key key,
+//   }) : super(key: key);
+//
+//   final VoidCallback onPressed;
+//
+//
+//
+//   @override
+//   Widget build (String buttonText) {
+//     return Expanded(
+//       child: OutlinedButton(
+//
+//         child: Text(buttonText,
+//           style: const TextStyle(
+//               fontSize: 16.0,
+//               fontWeight: FontWeight.bold
+//           ),
+//         ),
+//         onPressed: onPressed, =>
+//             onPressed(buttonText),
+//       ),
+//     );
+//   }
+// }
